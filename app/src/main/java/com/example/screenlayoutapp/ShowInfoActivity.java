4f153648cb2b282833c6ba1541918cce329776bb
angelahe07 <90780383+angelahe07@users.noreply.github.com>
2023-08-24 12:55:14 -0500

    added new page

--- app/src/main/java/com/example/screenlayoutapp/ShowInfoActivity.java
+++ app/src/main/java/com/example/screenlayoutapp/ShowInfoActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class ShowInfoActivity extends AppCompatActivity {
 
@@ -15,8 +16,14 @@ public class ShowInfoActivity extends AppCompatActivity {
 
         // open the intent to get the data when the screen loads
         Intent intent = getIntent();
-        String receivedFullText = intent.getStringExtra("FULLNAME");
+        String receivedFullText = intent.getStringExtra("FULLTEXT");
         // log to see what was received
-        Log.i("Angela", "recieved: " + receivedFullText);
+        Log.i("Angela", "received: " + receivedFullText);
+
+        // get a reference to TextView
+        // set the text of TextView
+
+        TextView infoTV = findViewById(R.id.infoTextView);
+        infoTV.setText(receivedFullText);
     }
 }
