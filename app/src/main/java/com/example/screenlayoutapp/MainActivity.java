4307948ab1cb237742b16e76bd70e24b4c7a4acc
angelahe07 <90780383+angelahe07@users.noreply.github.com>
2023-09-14 21:39:47 -0500

    didn't add anything, deleted extra space to take screenshot

--- app/src/main/java/com/example/screenlayoutapp/MainActivity.java
+++ app/src/main/java/com/example/screenlayoutapp/MainActivity.java
@@ -6,12 +6,9 @@ import android.text.method.PasswordTransformationMethod;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
-
-
 public class MainActivity extends AppCompatActivity {
     EditText password;
     CheckBox showpwd;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
